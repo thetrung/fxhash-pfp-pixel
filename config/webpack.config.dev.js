c6e187cd2782ec39bc7f79cb189f651546891ea9
Alexander Lamprecht <alex.lamprecht1@gmail.com>
2022-02-06 17:35:43 +0100

    fix infinite dev server reloads

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -3,6 +3,11 @@ const config = require("./webpack.config")
 module.exports = {
   ...config,
   mode: "development",
+  watchOptions: {
+    ignored: [
+      '**/tmp'
+    ]
+  },
   devServer: {
     // disables the Hot Module Replacement feature because probably not ideal
     // in the context of generative art
